1a6c34c6de09d0aaa7db4edbe835419f93c92420
BenoitCorsini <benoitcorsini@gmail.com>
2021-08-25 14:38:23 -0400

    config file added

---
+++ World/config.py
@@ -0,0 +1,43 @@
+PARAMS = {
+    'figure' : {
+        'size' : 10,
+        'extra_space' : 1,
+    },
+    'globe' : {
+        'land_colour' : 'forestgreen',
+        'water_colour' : 'aquamarine',
+    },
+    'shade' : {
+        'land_colour' : 'bisque',
+        'water_colour' : 'linen',
+        'angle' : 170,
+        'scale' : 0.7,
+        'ratio' : 0.3,
+        'rotation' : -75,
+        'x_pos' : 0.9,
+        'y_pos' : 0.1,
+    },
+    'airports' : {
+        'colour' : 'royalblue',
+        'size' : 0.1,
+        'border' : 1,
+        'border_colour' : 'midnightblue',
+    },
+    'flights' : {
+        'max_height' : 1.2,
+        'delta_step' : 0.01,
+        'size' : 1,
+        'colour' : 'gold',
+        'border' : 1,
+        'border_colour' : 'midnightblue',
+    },
+    'zorder' : {
+        'water' : 0.5,
+        'land' : 1,
+        'shade_water' : -1,
+        'shade_land' : -0.5,
+        'airports' : 1.5,
+        'flights' : 2,
+        'flights_border' : 1.9,
+    },
+}
